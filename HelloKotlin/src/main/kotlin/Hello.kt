c73ad5da41d9c784ea884dce1bc414c22c9936e6
gejun <gejun@droi.com>
2022-06-14 17:28:15 +0800

    fix string moban

--- HelloKotlin/src/main/kotlin/Hello.kt
+++ HelloKotlin/src/main/kotlin/Hello.kt
@@ -11,15 +11,15 @@ fun main() {
 //    println(Season1.SUMMER)
 //    println(Season1.AUTUMN)
 //    println(Season1.WINTER)
+    isRunYear(2008)
+    isRunYear(1998)
 
 }
 
-fun isRunYear(year:Int):Boolean{
-    var result = false
+fun isRunYear(year:Int){
     if((year % 4 == 0 && year % 100 != 0) || year % 400 ==0){
-       println(year + "is run nian")
+       println("$year  is run nian")
     } else {
-        println($(year) "is not run nian")
+        println("$year is not run nian")
     }
-    return result
 }

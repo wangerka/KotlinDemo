e4fe2d120f7bdf37c25a75b4683ce0d26825ac4b
gejun <gejun@droi.com>
2022-06-14 19:16:59 +0800

    when biaodashi

--- HelloKotlin/src/main/kotlin/Hello.kt
+++ HelloKotlin/src/main/kotlin/Hello.kt
@@ -2,24 +2,28 @@ fun main() {
     val name = 3
     println(name)
 
-    println(Season.SPRING)
-    println(Season.SUMMER)
-    println(Season.AUTUMN)
-    println(Season.WINTER)
-//
-//    println(Season1.SPRING)
-//    println(Season1.SUMMER)
-//    println(Season1.AUTUMN)
-//    println(Season1.WINTER)
     isRunYear(2008)
     isRunYear(1998)
+    isRunYear1(2008)
+    isRunYear1(1998)
+    println(add(3,4))
 
 }
 
 fun isRunYear(year:Int){
     if((year % 4 == 0 && year % 100 != 0) || year % 400 ==0){
-       println("$year  is run nian")
+       println("$year   is run nian")
     } else {
         println("$year is not run nian")
     }
 }
+
+fun isRunYear1(year:Int){
+    when{
+        year % 4 == 0 && year % 100 != 0 -> println("$year   is run nian")
+        year % 400 == 0 -> println("$year  is run nian")
+        else -> println("$year is not run nian")
+    }
+}
+
+fun add(age1:Int, age2:Int):Int = age1 + age2
